5e70f75842758274fcc14b92e74f55d84981782a
raulrequenac <raulrequec@hotmail.com>
2019-11-12 19:46:30 +0100

    done

--- config/routes.js
+++ config/routes.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/base.controller');
+const controller = require("../controllers/base.controller");
 
-router.get('/', controller.base);
-router.get('/celebrities', controller.celebrities);
-router.get('/celebrities/new', controller.createCelebrity);
-router.post('/celebrities/new', controller.doCreateCelebrity);
-router.get('/celebrities/:id', controller.celebrity);
-router.get('/celebrities/:id/edit', controller.editCelebrity);
-router.post('celebrities/:id/edit', controller.doEditCelebrity);
-router.post('/celebrities/:id/delete', controller.deleteCelebrity);
-router.get('/movies', controller.movies);
-router.get('/movies/new', controller.createMovie);
-router.post('/movies/new', controller.doCreateMovie);
-router.get('/movies/:id', controller.movie);
-router.get('/movies/:id/edit', controller.editMovie);
-router.post('movies/:id/edit', controller.doEditMovie);
-router.post('/movies/:id/delete', controller.deleteMovie);
+router.get("/", controller.base);
+router.get("/celebrities", controller.celebrities);
+router.get("/celebrities/new", controller.createCelebrity);
+router.post("/celebrities/new", controller.doCreateCelebrity);
+router.get("/celebrities/:id", controller.celebrity);
+router.get("/celebrities/:id/edit", controller.editCelebrity);
+router.post("/celebrities/:id/edit", controller.doEditCelebrity);
+router.post("/celebrities/:id/delete", controller.deleteCelebrity);
+router.get("/movies", controller.movies);
+router.get("/movies/new", controller.createMovie);
+router.post("/movies/new", controller.doCreateMovie);
+router.get("/movies/:id", controller.movie);
+router.get("/movies/:id/edit", controller.editMovie);
+router.post("/movies/:id/edit", controller.doEditMovie);
+router.post("/movies/:id/delete", controller.deleteMovie);
 
 module.exports = router;
