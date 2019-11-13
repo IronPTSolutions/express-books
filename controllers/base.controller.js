d9dd9092a5dbddf3b7dcac4380b95bb10257b8b1
Jeremy Mendoza <jeremy.mendoza@decathlon.com>
2019-11-13 11:24:14 +0100

    done 12 iterations

--- controllers/base.controller.js
+++ controllers/base.controller.js
@@ -1,40 +1,160 @@
 const createError = require('http-errors');
 const mongoose = require('mongoose');
-const celebrity = require('../models/celebrity.model')
-const movie = require('../models/movie.model')
+const Celebrity = require('../models/celebrity.model')
+const Movie = require('../models/movie.model')
 
 module.exports.base = (req, res, next) => {
     res.render('index', {
-        title: 'Welcome to your CRUD project'
+        title: 'Mongoose Movies LAB'
     });
 };
 
 module.exports.listCelebrities = (req, res, next) => {
-    celebrity.find()
+    Celebrity.find()
         .then(celebrity => res.render('celebrities/index.hbs', {celebrity}))
         .catch(err => next(err))
 };
 
-module.exports.celebrityDetail = (req, res, next) => {
-    celebrity.findOne({_id: req.params.celebrityId})
-        .then(celebrityDetail => {
-            res.render('celebrities/show.hbs', celebrityDetail) 
-        })
-        .catch(err => next(err))
+module.exports.detailCelebrity = (req, res, next) => {
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        next(createError(404));
+    } else {
+        Celebrity.findOne({_id: req.params.id})
+            .then(celebrity => {
+                res.render('celebrities/show.hbs', celebrity) 
+            })
+            .catch(err => next(err))
+    }
 };
 
+module.exports.editCelebrity = (req, res, next) => {
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        next(createError(404));
+    } else {
+        Celebrity.findOne({_id: req.params.id})
+            .then(celebrity => {
+                res.render('celebrities/form', celebrity) 
+            })
+            .catch(err => next(err))
+    }
+}
+
+module.exports.doEditCelebrity = (req, res, next) => {
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        next(createError(404));
+    } else {
+        Celebrity.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            { new: true }
+        )
+            .then(() => {
+                console.log(req.body)
+                res.redirect(`/celebrities/${req.params.id}`)
+            })
+            .catch(err => next(err))
+    }
+}
+
+module.exports.addCelebrities = (req, res, next) => {
+    res.render('celebrities/form', {
+        celebrity: new Celebrity
+    })
+}
+
+module.exports.doAddCelebrities = (req, res, next) => {
+    const newCelebrity = new Celebrity(req.body)
+
+    newCelebrity.save()
+        .then(newCelebrity => {
+            res.redirect('/celebrities')
+        })
+        .catch(err => next(err))
+}
+
+module.exports.deleteCelebrity = (req, res, next) => {
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        next(createError(404));
+    } else {
+        Celebrity.findByIdAndDelete(req.params.id)
+            .then(() => {
+                res.redirect('/celebrities')
+            })
+            .catch(err => next(err))
+    }
+}
+
 module.exports.listMovies = (req, res, next) => {
-    movie.find()
+    Movie.find()
         .then(movie => res.render('movies/index.hbs', {movie}))
         .catch(err => next(err))
 };
 
 module.exports.movieDetail = (req, res, next) => {
-    console.log(req.params.movieId)
-    movie.findOne({_id: req.params.movieId})
-        .then((movieDetail) => {
-            res.render('movies/show.hbs', movieDetail)
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        next(createError(404));
+    } else {
+        Movie.findOne({_id: req.params.id})
+            .then(movie => {
+                res.render('movies/show.hbs', movie)
+            })
+            .catch(err => next(err))
+    }
+}
+
+module.exports.editMovie = (req, res, next) => {
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        next(createError(404));
+    } else {
+        Movie.findOne({_id: req.params.id})
+            .then(movie => {
+                res.render('movies/form.hbs', movie) 
+            })
+            .catch(err => next(err))
+    }
+}
+
+module.exports.doEditMovie = (req, res, next) => {
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        next(createError(404));
+    } else {
+        Movie.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            { new: true }
+        )
+            .then((movie => {
+                console.log(movie)
+                res.redirect(`/movies/${req.params.id}`)
+            }))
+            .catch(err => next(err))
+    }
+}
+
+module.exports.addMovies = (req, res, next) => {
+    res.render('movies/form', {
+        movie: new Movie
+    })
+}
+
+module.exports.doAddMovies = (req, res, next) => {
+    const newMovie = new Movie(req.body)
+
+    newMovie.save()
+        .then(newMovie => {
+            res.redirect('/movies')
         })
         .catch(err => next(err))
+}
 
+module.exports.deleteMovie = (req, res, next) => {
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        next(createError(404));
+    } else {
+        Movie.findByIdAndDelete(req.params.id)
+            .then(() => {
+                res.redirect('/movies')
+            })
+            .catch(err => next(err))
+    }
 }
